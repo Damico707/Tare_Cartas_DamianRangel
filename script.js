c6988c3c61526aefac6033b72808a7943b1e4251
Damico707 <damian707rangel@gmail.com>
2026-02-25 07:56:27 -0500

    Design complete

---
+++ script.js
@@ -0,0 +1,7 @@
+let get= "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
+let card = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+let shuffle = 'https://deckofcardsapi.com/api/deck/new/shuffle/?remaining=true'
+
+console.log(get)
+console.log(card)
+console.log(shuffle)
