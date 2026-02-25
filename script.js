5eac64ee624863100e9a14937a4e053e6452c8a5
Damico707 <damian707rangel@gmail.com>
2026-02-25 08:55:49 -0500

    santi ojala seas millonario

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let get= "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
-let card = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
-let shuffle = 'https://deckofcardsapi.com/api/deck/new/shuffle/?remaining=true'
+let deckID = "ab68oud17jgy";
+let barajaNueva= "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+let card = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=10`
+let barajar = 'https://deckofcardsapi.com/api/deck/mjfbcgiqjk55/shuffle/'
 
-console.log(get)
-console.log(card)
-console.log(shuffle)
+
+async function dameBarajas() {
+    const respuesta = await fetch(barajaNueva)
+    const data = await respuesta.json()
+    return data.deck_id
+}
+
+
+
+async function dameCartas() {
+    const respuesta = await fetch(card)
+    const cartas = await respuesta.json()
+    return cartas
+}
+document.querySelector("div").addEventListener("click", async () =>{
+    deckID = await dameBarajas()
+    console.log(deckID)
+})
+
+document.querySelector("#end").addEventListener("click", async () =>{
+    cartas = await dameCartas()
+    console.table(cartas.cards)
+    document.querySelector("#end").textContent = cartas.cards[0].code
+})
